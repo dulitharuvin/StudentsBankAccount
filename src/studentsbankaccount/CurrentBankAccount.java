b41e2517e6cda711569a3a4db131a5bdaad3eab3
Dulitha Ruvin Daluwatta <dulitha.ruvin@gmail.com>
2018-07-16 01:54:58 +0530

    initial commit

---
+++ src/studentsbankaccount/CurrentBankAccount.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentsbankaccount;
+
+/**
+ *
+ * @author dulit
+ */
+public class CurrentBankAccount implements BankAccount{
+
+    @Override
+    public int getBalance() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getAccountNumber() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getAccountHolder() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deposit(Transaction t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void withdrawal(Transaction t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isOverdrawn() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void printStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/studentsbankaccount/Transaction.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentsbankaccount;
+
+/**
+ *
+ * @author dulit
+ */
+class Transaction {
+
+    private final String CustomerID;
+    private final int amount;
+
+    public Transaction(String CID, int amount) {
+        this.CustomerID = CID;
+        this.amount = amount;
+    }
+
+    public String getCID() {
+        return CustomerID;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String toString() {
+        return new String("[ "
+                + "Customer: " + CustomerID + ", "
+                + "Amount: " + amount
+                + "]");
+    }
+
+}
