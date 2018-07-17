948fce6ae9cbda890f358d1fce7e19b7dcaf40e5
Dulitha Ruvin Daluwatta <dulitha.ruvin@gmail.com>
2018-07-17 23:08:10 +0530

    added two more transactions to the student class

--- src/studentsbankaccount/Student.java
+++ src/studentsbankaccount/Student.java
@@ -29,23 +29,31 @@ public class Student extends Thread {
     @Override
     public void run() {
         try {
-            
+
             Transaction lottery = new Transaction(getName(), 10000);
             account.deposit(lottery);
             Thread.sleep(2000);
 
-            Transaction lottery2 = new Transaction(getName(), 20000);
-            account.withdrawal(lottery2);
+            Transaction iPadBuy = new Transaction(getName(), 20000);
+            account.withdrawal(iPadBuy);
             Thread.sleep(2000);
 
-            Transaction lottery3 = new Transaction(getName(), 10000);
-            account.deposit(lottery3);
+            Transaction lottery2 = new Transaction(getName(), 10000);
+            account.deposit(lottery2);
             Thread.sleep(2000);
 
-            Transaction lottery4 = new Transaction(getName(), 1444);
-            account.withdrawal(lottery4);  
-            Thread.sleep(2000);   
-            
+            Transaction iPhoneBuy = new Transaction(getName(), 1444);
+            account.withdrawal(iPhoneBuy);
+            Thread.sleep(2000);
+
+            Transaction present = new Transaction(getName(), 10000);
+            account.deposit(present);
+            Thread.sleep(2000);
+
+            Transaction macBookBuy = new Transaction(getName(), 1444);
+            account.withdrawal(macBookBuy);
+            Thread.sleep(2000);
+
         } catch (InterruptedException ex) {
             System.out.println(ex.toString());
         }
